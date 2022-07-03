b29f8162f6e78d8cae2a9923d099f06fbca1440b
Nathaniel Robertson <nat@nrobertson.net>
2022-07-03 16:17:28 -0700

    hunt_and_enter: add more sleep

--- custom-functions.py
+++ custom-functions.py
@@ -7,20 +7,23 @@ class NatCustomFunctions:
     def hunt_and_enter(text: str,occurrence: int = 0):
         """Searches for text in browser, exits the search box, presses enter to activate link result)"""
         actions.key("cmd-f")
-        # actions.sleep("200ms")
+        actions.sleep("300ms")
         actions.key("backspace")
+        actions.sleep("300ms")
         actions.insert("keyword_clear")
+        actions.sleep("300ms")
         actions.key("cmd-a")
+        actions.sleep("300ms")
         actions.key("backspace")
-        # actions.sleep("200ms")
+        actions.sleep("300ms")
         actions.insert(text)
-        actions.sleep("200ms")
+        actions.sleep("300ms")
         counter = 0
         while counter < occurrence - 1:
             # actions.sleep("200ms")
             actions.key("enter")
             counter += 1
-        # actions.sleep("200ms")
+        actions.sleep("300ms")
         actions.key("escape")
-        # actions.sleep("200ms")
+        actions.sleep("300ms")
         actions.key("enter")
