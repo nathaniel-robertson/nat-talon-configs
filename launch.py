f94d775850b3bb8e13d7770876b1e328549073bc
Nathaniel Robertson <nat@nrobertson.net>
2023-10-15 09:54:15 -0700

    update startup actions with new eye tracking functions

--- launch.py
+++ launch.py
@@ -1,6 +1,9 @@
+# This file is responsible for some startup actions
+# Such as turning on zoom mouse and turning off the mouse cross guide 
+
 from talon import app, actions
 def app_launched():
     actions.user.mouse_guide_toggle()
-    actions.user.mouse_toggle_zoom_mouse()
+    actions.tracking.control_zoom_toggle()
 
 app.register("launch", app_launched)
